a927346465a2ef241cd404648a904cb5482d36c7
alvesfcn <alvesfcn@gmail.com>
2025-03-15 11:02:57 -0300

    Update models.py

--- models.py
+++ models.py
@@ -4,22 +4,22 @@ class Email(Base):
     __tablename__ = "emails"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
     usuario_id = Column(Integer,ForeignKey ("usuarios.id"), index=True)
-    email = Column(String)
+    email = Column(String, unique=True, index=True)
 
 class Usuario(Base):
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    senha = Column(String)
+    email = relationship("Email", uselist="False", backref="usarios")
+    senha = relationship("Senha", uselist="False", backref="usuarios")
 
 class Senha(Base):  
     __tablename__ = "senhas"
 
     id = Column(Integer, primary_key=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), index=True)
     senha = Column(Integer) 
 
 
