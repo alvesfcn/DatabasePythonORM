8318818690f6036a7a0ad404c481fa285b0199f1
alvesfcn <alvesfcn@gmail.com>
2025-03-15 11:10:22 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,25 +1,30 @@
 from sqlalchemy import Column, Integer, String,ForeignKey
 from database import Base
+
 class Email(Base):
+ 
     __tablename__ = "emails"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    usuario_id = Column(Integer,ForeignKey ("usuarios.id"), index=True)
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer,ForeignKey ("usuarios.id"))
     email = Column(String)
 
+class Senha(Base):  
+  
+    __tablename__ = "senhas"
+    
+    id = Column(Integer, primary_key=Tru)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    senha = Column(Integer) 
+
 class Usuario(Base):
+ 
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    senha = Column(String)
+    email = relationship("Mail", uselist=False, backref="usuarios")
+    senha = relationship("Senha", uselist=False, backref="usuarios")
 
-class Senha(Base):  
-    __tablename__ = "senhas"
-
-    id = Column(Integer, primary_key=True, index=True)
-    senha = Column(Integer) 
 
 
